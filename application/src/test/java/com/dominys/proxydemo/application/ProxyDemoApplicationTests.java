defa3792510fa62f1a8eebc3985fb024a2843b9a
rbeapalko <roman.bezpalko@kenshoo.com>
2020-02-07 00:13:47 +0200

    First commit. Empty Spring Boot application

---
+++ application/src/test/java/com/dominys/proxydemo/application/ProxyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominys.proxydemo.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
